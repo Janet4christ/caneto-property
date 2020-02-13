1d2a6b8999ef1ad7880b18c65285f2c1f640edec
Janet4christ <60395951+Janet4christ@users.noreply.github.com>
2020-02-13 03:24:32 +0100

    comment removed

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,5 @@ $(document).ready(function() {
 		$(document).ready(function() {
 			$('.select2').select2();
 		});
-	//var imported = document.createElement('script');imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-120909275-1';document.head.appendChild(imported);window.dataLayer = window.dataLayer || [];
-	//function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-120909275-1');
+	
 });
